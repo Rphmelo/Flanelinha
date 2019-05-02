580c459f376cc160f731023297479be077d53e73
adrianomarinheiro <adrianomarinheiro@gmail.com>
2019-05-02 17:48:17 -0300

    adiciona recyclerview e telaa de cadastro dos carros

---
+++ app/src/main/java/br/com/flanelinha/app/cars/list/model/ui/CadastrarCarrosActivity.kt
@@ -0,0 +1,12 @@
+package br.com.flanelinha.app.cars.list.model.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.flanelinha.app.R
+
+class CadastrarCarrosActivity  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (R.layout.cadastrar_carros_activity)
+    }
+}
